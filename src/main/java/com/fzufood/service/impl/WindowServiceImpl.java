d0e4f8269b859a19188537941d5a9d702081b340
Your Name <you@example.com>
2020-11-15 17:48:39 +0800

    部分服务层实现

--- src/main/java/com/fzufood/service/impl/WindowServiceImpl.java
+++ src/main/java/com/fzufood/service/impl/WindowServiceImpl.java
@@ -16,11 +16,33 @@ public class WindowServiceImpl implements WindowService {
     }
     @Override
     public List<WindowEntry> list(Integer windowId, Integer userId) {
-        return null;
+        List<Window> windowList = userMapper.listLikeWindowsById(userId);
+        List<WindowEntry> windowEntries = new ArrayList<>();
+        for(Window window : windowList){
+            WindowEntry windowEntry = new WindowEntry();
+            windowEntry.setWindow(window.getWindow());
+            windowEntry.setCanteenName(window.getCanteenName());
+            windowEntry.setTag(window.getTag());
+            windowEntry.setStar(countStarsOnWindow(window.getWindowId()));
+            windowEntry.setIsMarked(window.isMarked);
+            windowEntries.add(windowEntry);
+        }
+        return windowEntries;
+    }
+    /**
+     * 根据dishId，查出该道菜品星级
+     */
+    private Double countStarsOnWindow(Integer windowId){
+        List<WindowComment> windowComments = windowCommentMapper.listWindowCommentsByWindowId(windowId);
+        Double stars = 0.0;
+        for(WindowComment windwoComment : windowComments){
+            stars += windowComment.getStars();
+        }
+        return stars/windowComments.size();
     }
     @Override
     public List<WindowEntry> getMarkedWindow(Integer userId) {
-        return null;
+
     }
     @Override
     public Integer updateMarkedWindow( Integer userId, Integer windowId)  {

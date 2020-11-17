124417eeaa5207c26ae21fc39d5d91585de921e2
qizong007 <978451432@qq.com>
2020-11-17 23:38:55 +0800

    增添dish的windowName

--- src/main/java/com/fzufood/service/impl/DishServiceImpl.java
+++ src/main/java/com/fzufood/service/impl/DishServiceImpl.java
@@ -132,7 +132,7 @@ public class DishServiceImpl implements DishService {
         dishInfo.setPrice(dish.getPrice());
         dishInfo.setStar(countStarsOnDish(dishId));
         dishInfo.setStarNum(countStarsNumOnDish(dishId));
-        // TODO: windowName
+        dishInfo.setWindowName(dish.getWindow().getWindowName());
         dishInfo.setTagList(dish.getTags());
         jsonObject.setData(dishInfo);
         jsonObject.setCode(code);
